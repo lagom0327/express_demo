e7c391eae54925514e3e0efb4c81bf65cb222023
lagom032 <lagom0327@gmail.com>
2021-02-02 07:04:46 +0800

    sequelize cli init

--- orm.js
+++ orm.js
@@ -1,14 +1,40 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 const dbData = require("./db");
-const { database, user, password } = dbData;
+const { database, user, host, password } = dbData;
 const sequelize = new Sequelize(database, user, password, {
-  host: "localhost",
+  host: host,
   dialect: "mysql",
 });
 
-try {
-  sequelize.authenticate();
-  console.log("Connection has been established successfully.");
-} catch (error) {
-  console.error("Unable to connect to the database:", error);
-}
+const User = sequelize.define("User", {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+  },
+});
+
+const Comment = sequelize.define("Comment", {
+  content: {
+    type: DataTypes.STRING,
+  },
+});
+// 一個使用者有很多的 comment
+// 自動在 comment 裡加上 userId
+User.hasMany(Comment);
+Comment.belongsTo(User);
+sequelize.sync().then(() => {
+  // console.log("creadted done", res);
+  Comment.findOne({
+    where: {
+      id: 2,
+    },
+    include: User,
+  }).then((comment) => {
+    console.log("comment", JSON.stringify(comment, null, 2));
+  });
+});
+
+// `sequelize.define` also returns the model
